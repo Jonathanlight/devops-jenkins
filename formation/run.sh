b1de598be12b9a6a72a21d83408d53e4c2871c5a
ubuntu <jonathan.kablan@gmail.com>
2019-02-26 14:26:51 +0100

    ft-config-v1

---
+++ formation/run.sh
@@ -0,0 +1,28 @@
+
+# Checked if folder isset or create folder
+#------------------------------------------------------
+if [ ! -d $(pwd)/data ]
+then
+	mkdir $(pwd)/data
+fi	
+
+# Checked if folder isset or create folder
+#-------------------------------------------------------
+if [ ! -d $(pwd)/data/jenkins ]
+then
+	mkdir $(pwd)/data/jenkins
+fi
+
+# Set right file
+#--------------------------------------------------------
+chown 1000 -R $(pwd)/data/jenkins
+
+# Stop all container + clean logs
+#---------------------------------------------------------
+docker-compose stop
+truncate -s 0 /var/lib/docker/containers/*/*-json.log
+
+# Restart all containers
+#----------------------------------------------------------
+docker-compose up --build -d
+
